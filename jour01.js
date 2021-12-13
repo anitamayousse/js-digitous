67f22d43e90b9c6728a1862da466bff8678a9bf5
Apple <anita.mayousse@gmail.com>
2021-12-13 15:25:31 +0100

    2nd

---
+++ jour01.js
@@ -0,0 +1,16 @@
+// 01 - Hello World
+console.log("Hello World!");
+// 02 - String
+var test="test";
+test ="My name is Anita";
+console.log(test);
+// 03 - Concatenation
+var name ="Anita";
+console.log("Nice to meet you " + name);
+// 04 - String Length
+var testlength ="I'm very long !";
+console.log(testlength.length);
+// 05 - Replace
+var food = "croissant is meh";
+food = food.replace("meh","so good");
+console.log(food);
