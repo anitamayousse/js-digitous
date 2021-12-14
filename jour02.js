b24831da5c684066e0230d440d560050edfa55bd
Apple <anita.mayousse@gmail.com>
2021-12-14 14:32:58 +0100

    3rd

--- jour02.js
+++ jour02.js
@@ -6,3 +6,21 @@ console.log(integer,float);
 let basic = 34;
 let stringified = basic.toString();
 console.log(stringified);
+//03 - Round
+let rounded = Math.round(1.5);
+console.log(rounded);
+//04 - Arithmétique 
+let test = 12;
+let bis = 5;
+console.log (test + bis);
+//05 - Comparaison 
+let test1 = 143;
+let bis1 = 219;
+console.log(test1 > bis1);
+//06 - Condition 
+let limit = 50;
+let score = 64;
+if ( score >= limit){
+    console.log("OK good!");
+} else {
+    console.log("Oh nooo...");}
