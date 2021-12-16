0ce60dac42786174192449ea6ea9041cdb49fdba
Apple <anita.mayousse@gmail.com>
2021-12-16 14:10:22 +0100

    1st

---
+++ jour04/02Combine.js
@@ -0,0 +1,19 @@
+const cat = {
+    name: "Garfield",
+    age: 3,
+    isCute: true
+};
+
+const cat2 = {
+    name: "Bob",
+    age: 5,
+    isCute: false
+};
+const cats = {
+cat, cat2
+}
+
+console.log(cat.age);
+console.log(cat["age"]);
+console.log(cat2.isCute);
+console.log(cat2["isCute"]);
