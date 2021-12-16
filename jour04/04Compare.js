f06759f7676db1f821ad511a57724253ace818b3
Apple <anita.mayousse@gmail.com>
2021-12-16 14:53:10 +0100

    1st

---
+++ jour04/04Compare.js
@@ -0,0 +1,13 @@
+function compare(num1,num2) {
+    if (num1 > num2){
+        console.log(num1,"is bigger");
+    }
+    else if (num2 > num1){
+        console.log(num2 ,"is bigger");
+    }
+    else if (num1 == num2){
+        console.log("both are the same");
+    }
+}
+
+compare(3,4);
