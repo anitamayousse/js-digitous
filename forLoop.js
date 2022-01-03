e4dac3f865c42f381f193849d4889a82ff9cf685
Apple <anita.mayousse@gmail.com>
2022-01-03 13:54:56 +0100

    2nd

---
+++ forLoop.js
@@ -0,0 +1,43 @@
+/*01 - Somme des carrés*/
+function SquareandSum(num) {
+
+    var sum = 0;
+    for (var i = 0, x = num.length; i <x; i++) {
+      sum += num[i] * num[i];
+    }
+    console.log(sum);
+    return sum;
+  }
+
+SquareandSum([5,6,7,8,9,10])
+/*02 - Comptons*/
+/*03 - Chanceux*/
+let i;
+for (let i=0; i<20; i++){
+  val = Math.floor(Math.random()*6+1);
+  if(val >= 5); {
+    console.log(val);
+  }
+}
+
+/*04 - Des boites*/
+10
+34
+17
+30
+14
+/*05 - Des setiob*/
+12
+24
+33
+39
+42
+0
+/*06 - Encore des boites*/
+0
+3
+-3
+2
+-2
+5
+-4
