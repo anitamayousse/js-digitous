fc82cd0d681b215e0d53fd9c224d80ae6e38d007
Apple <anita.mayousse@gmail.com>
2022-01-06 17:02:54 +0100

    2nd

---
+++ jour06/age.js
@@ -0,0 +1,19 @@
+function calculateAge(year, month, day) {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    var currentMonth = currentDate.getUTCMonth() + 1;
+    var currentDay = currentDate.getUTCDate();
+  
+    var age = currentYear - year;
+    if (currentMonth > month) {
+        return age;
+    } else {
+        if (currentDay >= day) {
+            return age;
+        } else {
+            age--;
+            return age;
+        }
+    }
+}
+console.log(calculateAge(1991, 26, 12));
