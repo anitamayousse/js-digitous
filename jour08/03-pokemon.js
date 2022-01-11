a9bc385dc47a642c40040b455ac63fe82bdac5b8
Apple <anita.mayousse@gmail.com>
2022-01-11 17:10:01 +0100

    3rd

--- jour08/03-pokemon.js
+++ jour08/03-pokemon.js
@@ -2,4 +2,16 @@ const axios = require("axios");
 
     axios.get(" https://pokeapi.co/api/v2/pokemon/ditto").then(function (catchPokemon) {
         console.log(`id: ${catchPokemon.data.id}, name: ${catchPokemon.data.name}`);
-  });
+  })
+
+  .catch(function(err) {
+    console.log("Error!");
+  })
+
+  axios.get(" https://pokeapi.co/api/v2/pokemon/132").then(function (catchPokemon) {
+    console.log(`id: ${catchPokemon.data.id}, name: ${catchPokemon.data.name}`);
+})
+
+.catch(function(err) {
+console.log("Error!");
+})
