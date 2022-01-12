57768384bcc575b1feaa52ec9b020539d58ce727
Apple <anita.mayousse@gmail.com>
2022-01-12 10:07:52 +0100

    6th

--- jour08/01-countries.js
+++ jour08/01-countries.js
@@ -4,6 +4,6 @@ axios.get("https://restcountries.com/v3.1/all").then(function (res) {
     let countriesNames = res.data.map (function (getCountries) {
     return getCountries.name.common;
 })
-console.log (countriesNames);   
+console.log (countriesNames.join("-"));   
 });
 
